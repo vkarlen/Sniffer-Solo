d7f68b6bcc6b81a170690991020d6099fdd540c1
Vada Karlen <vada.karlen@gmail.com>
2021-03-17 14:06:31 -0500

    All of a user's pets are displayed on the pets page

--- src/components/MyPetsPage/MyPetsPage.jsx
+++ src/components/MyPetsPage/MyPetsPage.jsx
@@ -7,7 +7,20 @@ function MyPetsPage() {
     <div>
       <h2>My Pets</h2>
       <button>+ add a pet</button>
-      <div>woof</div>
+      {pets.length === 0 ? (
+        <p>Please add a pet!</p>
+      ) : (
+        <div>
+          {pets.map((pet) => {
+            return (
+              <div key={pet.id}>
+                <img src={pet.image_url} alt={pet.name} />
+                <p>{pet.name}</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
