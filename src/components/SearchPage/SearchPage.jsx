5b7d09acf748e13d38b8f7e89661f8aaac394021
Vada Karlen <vada.karlen@gmail.com>
2021-03-19 16:09:46 -0500

    Added page for Add Pet and started building the form to take in values

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import './SearchPage.css';
