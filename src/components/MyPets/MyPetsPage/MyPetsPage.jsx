935853f25ead3a5e4d52f5ee3cefe6e369e722f1
Vada Karlen <vada.karlen@gmail.com>
2021-03-21 22:29:55 -0500

    Added base styling to My Pets page

--- src/components/MyPets/MyPetsPage/MyPetsPage.jsx
+++ src/components/MyPets/MyPetsPage/MyPetsPage.jsx
@@ -1,36 +1,46 @@
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
+import { Grid, Paper, Container } from '@material-ui/core';
+
+import './MyPetsPage.css';
+
 function MyPetsPage() {
   const history = useHistory();
 
   const pets = useSelector((store) => store.user.userPets);
 
   return (
-    <div>
+    <Container maxWidth="md">
       <h2>My Pets</h2>
       <button onClick={() => history.push('/addapet')}>+ add a pet</button>
 
       {pets.length === 0 ? (
         <p>Please add a pet!</p>
       ) : (
-        <div>
+        <Grid container spacing={2}>
           {pets.map((pet) => {
             return (
-              <div
-                key={pet.id}
-                onClick={() => {
-                  history.push(`/pets/${pet.id}`);
-                }}
-              >
-                <img src={pet.image_url} alt={pet.name} />
-                <p>{pet.name}</p>
-              </div>
+              <Grid item xs={6} md={3} key={pet.id}>
+                <Paper
+                  className="petCard"
+                  onClick={() => {
+                    history.push(`/pets/${pet.id}`);
+                  }}
+                >
+                  <img
+                    src={pet.image_url}
+                    alt={pet.name}
+                    className="petImage"
+                  />
+                  <p>{pet.name}</p>
+                </Paper>
+              </Grid>
             );
           })}
-        </div>
+        </Grid>
       )}
-    </div>
+    </Container>
   );
 }
 
