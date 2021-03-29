704935335e3f9133c2cd1a58e44105b316c642d6
Vada Karlen <vada.karlen@gmail.com>
2021-03-29 14:14:27 -0500

    Added linked in link to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
-  return <footer>&copy; Vada Karlen</footer>;
+  return (
+    <footer>
+      <a href="https://www.linkedin.com/in/vadak/">&copy; Vada Karlen</a>
+    </footer>
+  );
 }
 
 export default Footer;
